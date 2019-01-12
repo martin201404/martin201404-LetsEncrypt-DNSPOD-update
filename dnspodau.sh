a9158a1feb828d04662405e30d33ec405c29521f
martin <fan.shuang.ming@foocaa.com>
2019-01-12 10:59:36 +0800

    修改sh 说明

--- dnspodau.sh
+++ dnspodau.sh
@@ -7,8 +7,7 @@ PATH=$(cd `dirname $0`; pwd)
 
 # 调用 python 脚本，自动设置 DNS TXT 记录。
 # 第一个参数：需要为那个域名设置 DNS 记录
-# 第二个参数：需要为具体那个 RR 设置
-# 第三个参数: letsencrypt 动态传递的 RR 值 
+# 第二个参数：letsencrypt 动态传递的 RR 值 
 
 echo $CERTBOT_DOMAIN  "_acme-challenge" $CERTBOT_VALIDATION
 
